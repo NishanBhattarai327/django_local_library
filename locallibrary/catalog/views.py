aa8419c4b7f4c954dbe1d77367fefe70f69d95a8
NishanBhattarai327 <bhattarainishan327@gmail.com>
2024-12-24 07:19:54 +0545

    Use session to no of visits to index page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -14,6 +14,10 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
     num_genres = Genre.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
     
 
     context = {
@@ -22,6 +26,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
