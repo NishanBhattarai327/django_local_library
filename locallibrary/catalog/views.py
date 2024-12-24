d865a179e781aa4c84b3ad74fb70344d5fcdad3d
NishanBhattarai327 <bhattarainishan327@gmail.com>
2024-12-24 06:57:45 +0545

    Generic List and detail views

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Book, BookInstance, Genre, Language, Author
+from django.views import generic
 
 # Create your views here.
 def index(request):
@@ -24,3 +25,17 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 5
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
